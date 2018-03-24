69ada52e6e9273d3fae762896941ed17306f1d8f
Mark Rose <markrose@acm.org>
2018-03-23 17:11:12 -0700

    Fix ability to move backward to achieve the goal
    Fixed the kinematic equations to more closely match Autonomous Mobile
    Robots. Augmented unit tests to include tests for moving backward.

--- src/diff_drive/goal_controller.py
+++ src/diff_drive/goal_controller.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import division, print_function
 from math import pi, sqrt, sin, cos, atan2
 from diff_drive.pose import Pose
 
@@ -10,12 +10,13 @@ class GoalController:
     def __init__(self):
         self.kP = 3
         self.kA = 8
-        self.kB = 1.5
+        self.kB = -1.5
         self.maxLinearSpeed = 1E9
         self.maxAngularSpeed = 1E9
         self.maxLinearAcceleration = 1E9
         self.linearTolerance = 0.025 # 2.5cm
         self.angularTolerance = 3/180*pi # 3 degrees
+        self.forwardMovementOnly = False
 
     def setConstants(self, kP, kA, kB):
         self.kP = kP
@@ -37,6 +38,9 @@ class GoalController:
     def setAngularTolerance(self, tolerance):
         self.angularTolerance = tolerance
 
+    def setForwardMovementOnly(self, forwardOnly):
+        self.forwardMovementOnly = forwardOnly
+
     def getGoalDistance(self, cur, goal):
         if goal is None:
             return 0
@@ -53,18 +57,63 @@ class GoalController:
 
     def getVelocity(self, cur, goal, dT):
         desired = Pose()
+
+        a = -cur.theta + atan2(goal.y - cur.y, goal.x - cur.x)
+
+        # In Automomous Mobile Robots, they assume theta_G=0. So for
+        # the error in heading, we have to adjust theta based on the
+        # (possibly non-zero) goal theta.
+        theta = cur.theta - goal.theta
+        b = -theta - a
+
         d = self.getGoalDistance(cur, goal)
-        a = atan2(goal.y - cur.y, goal.x - cur.x) - cur.theta
-        b = cur.theta + a - goal.theta
-        
+        if self.forwardMovementOnly:
+            direction = 1
+            a = self.normalizePi(a)
+            b = self.normalizePi(b)
+            #if abs(a) > pi/2:
+            #    b = 0
+        else:
+            direction = self.sign(cos(a))
+            a = self.normalizeHalfPi(a)
+            b = self.normalizeHalfPi(b)
+
         if abs(d) < self.linearTolerance:
             desired.xVel = 0
-            desired.thetaVel = -self.kB * b
+            desired.thetaVel = self.kB * theta
         else:
-            desired.xVel = self.kP * d
+            desired.xVel = self.kP * d * direction
             desired.thetaVel = self.kA*a + self.kB*b
 
-        # Adjust velocities if linear or angular rates or accel too high.
-        # TBD
-        
+        #print('current x:', cur.x, 'y:', cur.y, 'theta:', cur.theta,
+        #    '  goal x:', goal.x, 'y:', goal.y, 'theta:', goal.theta)
+        #print('  theta:', theta, 'a:', a, 'b:', b,
+        #    'v:', desired.xVel, 'w:', desired.thetaVel)
+        #print(str(cur.x) + ',' + str(cur.y) + ',' + str(cur.theta))
+
+        # TBD: Adjust velocities if linear or angular rates
+        # or acceleration too high.
+
         return desired
+
+    def normalizeHalfPi(self, alpha):
+        if alpha > pi/2:
+            return alpha - pi
+        elif alpha < -pi/2:
+            return alpha + pi
+        else:
+            return alpha
+
+    def normalizePi(self, alpha):
+        if alpha > pi:
+            return alpha - 2*pi
+        elif alpha < -pi:
+            return alpha + 2*pi
+        else:
+            return alpha
+
+    def sign(self, x):
+        if x >= 0:
+            return 1
+        else:
+            return -1
--- tests/test_goal_controller.py
+++ tests/test_goal_controller.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import division, print_function
 PKG='test_goal_controller'
 
 import unittest
@@ -12,12 +12,16 @@ class TestGoalController(unittest.TestCase):
     def setUp(self):
         self.controller = GoalController()
 
+    # Test that the robot does not move when already at the goal
+    # at the right heading.
     def testAtGoal(self):
         cur = Pose()
         desired = self.controller.getVelocity(cur, cur, 0.1)
         self.assertEquals(desired.xVel, 0)
         self.assertEquals(desired.thetaVel, 0)
 
+    # Test that a goal pose ahead of the current position at the same
+    # heading causes a straight-ahead move.
     def testStraightAhead(self):
         cur = Pose()
         goal = Pose()
@@ -26,6 +30,18 @@ class TestGoalController(unittest.TestCase):
         self.assertGreater(desired.xVel, 0)
         self.assertEquals(desired.thetaVel, 0)
 
+    # Test that a goal pose behind the current position at the same
+    # heading causes a straight-back move.
+    def testStraightBack(self):
+        cur = Pose()
+        goal = Pose()
+        goal.x = -1
+        desired = self.controller.getVelocity(cur, goal, 0.1)
+        self.assertLess(desired.xVel, 0)
+        self.assertEquals(desired.thetaVel, 0)
+
+    # Test that a goal at the current position with a leftward goal
+    # heading causes a leftward rotation.
     def testRotateLeft(self):
         cur = Pose()
         goal = Pose()
@@ -34,6 +50,8 @@ class TestGoalController(unittest.TestCase):
         self.assertEquals(desired.xVel, 0)
         self.assertGreater(desired.thetaVel, 0)
 
+    # Test that a goal at the current position with a rightward goal
+    # heading causes a rightward rotation.
     def testRotateRight(self):
         cur = Pose()
         goal = Pose()
@@ -42,49 +60,96 @@ class TestGoalController(unittest.TestCase):
         self.assertEquals(desired.xVel, 0)
         self.assertLess(desired.thetaVel, 0)
 
+    # Test that a goal pose that is reachable with a forward, leftward
+    # arc causes a forward movement with some leftward rotation.
     def testCurveLeft(self):
         cur = Pose()
         goal = Pose()
         goal.x = 1
         goal.y = 1
-        goal.theta = pi
+        goal.theta = pi/2
         desired = self.controller.getVelocity(cur, goal, 0.1)
         self.assertGreater(desired.xVel, 0)
         self.assertGreater(desired.thetaVel, 0)
 
+    # Test that a goal pose that is reachable with a forward, rightward
+    # arc causes a forward movement with some rightward rotation.
     def testCurveRight(self):
         cur = Pose()
-        cur.theta = pi
+        cur.theta = pi/2
         goal = Pose()
         goal.x = 1
         goal.y = 1
         desired = self.controller.getVelocity(cur, goal, 0.1)
         self.assertGreater(desired.xVel, 0)
+        self.assertLess(desired.thetaVel, 0)
+
+    # Test that a goal pose behind the robot that is reachable with a
+    # leftward arc causes a backward movement with some rightward
+    # rotation.
+    def testCurveBackLeft(self):
+        cur = Pose()
+        goal = Pose()
+        cur.x = 1
+        cur.y = 1
+        goal.theta = pi/2
+        desired = self.controller.getVelocity(cur, goal, 0.1)
+        self.assertLess(desired.xVel, 0)
+        self.assertGreater(desired.thetaVel, 0)
+
+    # Test that a goal pose behind the robot that is reachable with a
+    # rightward arc causes a backward movement with some leftward
+    # rotation.
+    def testCurveBackRigth(self):
+        cur = Pose()
+        goal = Pose()
+        cur.x = 1
+        cur.y = 1
+        cur.theta = pi/2
+        desired = self.controller.getVelocity(cur, goal, 0.1)
+        self.assertLess(desired.xVel, 0)
         self.assertLess(desired.thetaVel, 0)
 
     def testButtonHookLeft(self):
         cur = Pose()
         goal = Pose()
         goal.x = 1
+        goal.y = 1
         goal.theta = pi
         desired = self.controller.getVelocity(cur, goal, 0.1)
         self.assertGreater(desired.xVel, 0)
-        self.assertLess(desired.thetaVel, 0)
+        self.assertGreater(desired.thetaVel, 0)
 
     def testButtonHookRight(self):
         cur = Pose()
         goal = Pose()
         goal.x = 1
+        goal.y = -1
         goal.theta = -pi
         desired = self.controller.getVelocity(cur, goal, 0.1)
         self.assertGreater(desired.xVel, 0)
-        self.assertGreater(desired.thetaVel, 0)
+        self.assertLess(desired.thetaVel, 0)
 
     def testGoToGoal(self):
-        self.checkGoToGoal(0, 0, 0, 1, 1, pi/2)
-        self.checkGoToGoal(0, 0, 0, 1, 1, -pi/2)
-        self.checkGoToGoal(0, 0, 0, 0, 1, pi)
-        self.checkGoToGoal(0, 0, 0, 1, 0, pi)
+        self.checkGoToGoal(0, 0, 0, 1, 0, 0) # Straight ahead
+        self.checkGoToGoal(0, 0, 0, 1, 1, pi/2) # Arc left
+        self.checkGoToGoal(0, 0, 0, 1, 1, -pi/2) # Left, then turn right
+        self.checkGoToGoal(0, 0, 0, 0, 1, pi) # Go left
+        self.checkGoToGoal(0, 0, 0, 1, 0, pi) # Go ahead and u-turn
+        self.checkGoToGoal(0, 0, 0, -1, 0, 0) # Straight back
+        self.checkGoToGoal(0, 0, 0, -1, -1, 0) # Back up to right
+        self.checkGoToGoal(0, 0, 0, -1, -1, pi) # Back up and turn left
+
+    def testGoToGoalForwardOnly(self):
+        self.controller.setForwardMovementOnly(True)
+        self.checkGoToGoal(0, 0, 0, 1, 0, 0) # Straight ahead
+        self.checkGoToGoal(0, 0, 0, 1, 1, pi/2) # Arc left
+        self.checkGoToGoal(0, 0, 0, 1, 1, -pi/2) # Left, then turn right
+        self.checkGoToGoal(0, 0, 0, 0, 1, pi) # Go left
+        self.checkGoToGoal(0, 0, 0, 1, 0, pi) # Go ahead and u-turn
+        self.checkGoToGoal(0, 0, 0, -1, 0, 0) # Straight back
+        self.checkGoToGoal(0, 0, 0, -1, -1, 0) # Back up to right
+        self.checkGoToGoal(0, 0, 0, -1, -1, pi) # Back up and turn left
 
     def checkGoToGoal(self, x0, y0, th0, x1, y1, th1):
         dTol = 0.05 # 5cm
@@ -110,12 +175,16 @@ class TestGoalController(unittest.TestCase):
                 return
 
             desired = self.controller.getVelocity(cur, goal, dT)
-            cur.x += dT * desired.xVel * cos(cur.theta)
-            cur.y += dT * desired.xVel * sin(cur.theta)
-            cur.theta += dT * desired.thetaVel
+            newTheta = cur.theta + dT*desired.thetaVel
+            midTheta = (cur.theta + newTheta) / 2.0
+            cur.x += dT * desired.xVel * cos(midTheta)
+            cur.y += dT * desired.xVel * sin(midTheta)
+            cur.theta = newTheta
 
         # If we get here, we didn't reach the goal.
-        self.assertFalse('Did not reach the goal.')
+        self.assertFalse('Did not reach the goal: p0='
+                         + str((x0,y0,th0))
+                         + ' p1=' + str((x1,y1,th1)))
         
 if __name__ == '__main__':
     unittest.main()
